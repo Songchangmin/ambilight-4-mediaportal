27b716511aec3dab9529aa6c80c61f614d1b6868
rgazou2 <rgazou2@gmail.com>
2013-04-06 12:42:29 +0200

    Add source files  - Atmoduino 0.50 : https://bitbucket.org/RickDB/atmowin-atmoduino-mod/commits/31b7d141531e697f6b86f5f59d13fc88c3da352d  - Atmowin original 0.51

---
+++ Atmowin/Atmowin - Atmoduino/0.50/source/RickDB-atmowin-atmoduino-mod-31b7d141531e/AtmoClassicConfigDialog.cpp
@@ -0,0 +1,66 @@
+#include "StdAfx.h"
+#include "atmoclassicconfigdialog.h"
+#include "resource.h"
+
+CAtmoClassicConfigDialog::CAtmoClassicConfigDialog(HINSTANCE hInst, HWND parent, CAtmoConfig *pConfig) : 
+  CBasicDialog(hInst, IDD_CFG_ATMO_DLG, parent)
+{
+   this->m_pConfig = pConfig;
+}
+
+CAtmoClassicConfigDialog::~CAtmoClassicConfigDialog(void)
+{
+}
+
+
+ATMO_BOOL CAtmoClassicConfigDialog::InitDialog(WPARAM wParam) {
+
+        m_hCbxComports = getDlgItem(IDC_COMPORT);
+        InitDialog_ComPorts( m_hCbxComports );
+        int com = m_pConfig->getComport() - 1;
+        if(com < 0) com = 0;
+
+        ComboBox_SetCurSel(m_hCbxComports, com);
+
+		char buf[10];
+		HWND control;
+
+		control = getDlgItem(IDC_ATMOCL_LED_AMOUNT);
+		Edit_LimitText(control, 3);
+		sprintf(buf, "%d", m_pConfig->getAtmoClLeds());
+		Edit_SetText(control, buf);
+
+		return ATMO_FALSE;
+}
+
+
+ATMO_BOOL CAtmoClassicConfigDialog::ExecuteCommand(HWND hControl,int wmId, int wmEvent) {
+
+	switch(wmId) {
+	case IDOK: {
+		int comportSel = ComboBox_GetCurSel(getDlgItem(IDC_COMPORT));
+
+		HWND control = getDlgItem(IDC_ATMOCL_LED_AMOUNT);
+		char buf[10];
+		Edit_GetText(control, buf, sizeof(buf));
+		m_pConfig->setAtmoClLeds(atoi(buf));
+
+		m_pConfig->setComport(comportSel + 1);
+		
+		EndDialog(this->m_hDialog, wmId);
+		break;
+			   }
+
+        case IDCANCEL: {
+            // m_pBackupConfig --> wieder herstellen...
+           EndDialog(this->m_hDialog, wmId);
+           break;
+        }
+
+       default:
+           return ATMO_FALSE;
+
+    }
+
+    return ATMO_TRUE;
+}
